3e313a4618dcf15b8fe7aca3525e320776e0d8ae
Samuel <oiranca@gmail.com>
2022-04-20 12:42:37 +0100

    fix: change substring method

--- src/method/markdownToHtml.tsx
+++ src/method/markdownToHtml.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const markdownToHtml = (markDownText: string) => {
   const alphanumericCharacter = /\w/;
-  const regex = /^#\s/;
-  if (regex.test(markDownText)) {
-    return <h1>{markDownText.substring(markDownText.search(alphanumericCharacter))}</h1>;
+  const regexFirstHeader = /^#\s/;
+  if (regexFirstHeader.test(markDownText)) {
+    return <h1>{markDownText.slice(markDownText.search(alphanumericCharacter))}</h1>;
   }
   return markDownText;
 };
