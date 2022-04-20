cc86c59d3bc5aba6303241f1b08403ae2f893f5a
Samuel <oiranca@gmail.com>
2022-04-20 12:35:18 +0100

    fix: markdown test

--- src/method/markdownToHtml.spec.ts
+++ src/method/markdownToHtml.spec.ts
@@ -2,7 +2,7 @@ import { markdownToHtml } from './markdownToHtml';
 
 describe('Markdown style', () => {
   it('Is a h1 tag', () => {
-    const markdownText = '#This is a H1 tag';
+    const markdownText = '# This is a H1 tag';
     const method = markdownToHtml(markdownText);
     expect(method).toMatchObject({ type: 'h1', props: { children: 'This is a H1 tag' } });
   });
--- src/method/markdownToHtml.tsx
+++ src/method/markdownToHtml.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export const markdownToHtml = (markDownText: string) => {
+  const alphanumericCharacter = /\w/;
   const regex = /^#\s/;
   if (regex.test(markDownText)) {
-    return <h1>{markDownText.replace('#', '')}</h1>;
+    return <h1>{markDownText.substring(markDownText.search(alphanumericCharacter))}</h1>;
   }
   return markDownText;
 };
